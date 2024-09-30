a3c26cf09d38826cb1b5e020cf9f870b3387b8a5
gitbarnabedikartola <gitbarnabedikartola@gmail.com>
2024-09-29 23:46:51 -0300

    create FalaQueEuTeEscuto

---
+++ biglinux-make-iso-profiles/FalaQueEuTeEscuto/special-commands.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#add biglinux standed desktop to FalaQueEuTeEscuto desktop
+cat biglinux/kde/Packages-Desktop  >>  biglinux/FalaQueEuTeEscuto/Packages-Desktop
+
+#remove desktop
+mv biglinux/FalaQueEuTeEscuto/Packages-Desktop biglinux/FalaQueEuTeEscuto/Packages-Desktop-prov
+grep -v -f biglinux-make-iso-profiles/FalaQueEuTeEscuto/Desktop-remove  biglinux/FalaQueEuTeEscuto/Packages-Desktop-prov  >  biglinux/FalaQueEuTeEscuto/Packages-Desktop
+rm biglinux/FalaQueEuTeEscuto/Packages-Desktop-prov
+
+
+cp biglinux-make-iso-profiles/FalaQueEuTeEscuto/kaccessrc biglinux/FalaQueEuTeEscuto/root-overlay/etc/skel/.config/kaccessrc
